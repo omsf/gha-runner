0e6f570bd68653ca5590cbad2a66dd28a24fb789
Ethan Holz <ethan.holz@omsf.io>
2024-06-12 10:15:06 -0400

    test(main): added simple mocked tests for validating behavior

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,12 @@ import os
 from gha_runner.__main__ import (
     parse_aws_params,
     _env_parse_helper,
+    start_runner_instances,
+    stop_runner_instances,
 )
+from unittest.mock import Mock, patch, mock_open
+
+pytestmark = pytest.mark.main
 
 
 def test_env_parse_helper():
@@ -97,3 +102,100 @@ def test_parse_aws_params_empty():
         "home_dir": "",
         "region_name": "",
     }
+
+
+# Define a mock CloudProvider class (e.g., MockAWS)
+class MockCloudProvider:
+    def __init__(self, **kwargs):
+        pass
+
+    def create_instances(self):
+        return mock_get_instance_mapping()
+
+    def wait_until_ready(self, instance_ids):
+        pass
+
+    def remove_instances(self, instance_ids):
+        pass
+
+    def wait_until_removed(self, instance_ids):
+        pass
+
+
+def mock_get_instance_mapping():
+    return {"instance_id_1": "label_1"}
+
+
+@pytest.fixture
+def mock_cloud_deployment_factory():
+    with patch.dict(
+        "gha_runner.clouddeployment.CloudDeploymentFactory.providers",
+        {"mock_provider": MockCloudProvider},
+    ):
+        yield
+
+
+@pytest.fixture
+def mock_gh_output():
+    with patch.dict("os.environ", {"GITHUB_OUTPUT": "mock_output"}):
+        with patch("builtins.open", mock_open()):
+            yield
+
+
+@pytest.fixture
+def mock_gh():
+    gh_mock = Mock()
+    gh_mock.get_latest_runner_release.return_value = "mock_release"
+    gh_mock.create_runner_tokens.return_value = ["mock_token"]
+    gh_mock.remove_runner.return_value = None
+
+    return gh_mock
+
+
+def test_start_runner_instances_smoke(
+    mock_cloud_deployment_factory, mock_gh_output, mock_gh
+):
+    try:
+        start_runner_instances(
+            provider="mock_provider",
+            gh=mock_gh,
+            count=1,
+            cloud_params={},
+        )
+    except Exception as e:
+        pytest.fail(f"Exception raised: {e}")
+
+
+def test_stop_runner_instances_smoke(
+    mock_cloud_deployment_factory, mock_gh_output, mock_gh
+):
+    with patch(
+        "gha_runner.__main__.get_instance_mapping",
+        new=mock_get_instance_mapping,
+    ):
+        try:
+            stop_runner_instances(
+                provider="mock_provider", cloud_params={}, gh=mock_gh
+            )
+        except Exception as e:
+            pytest.fail(f"stop_runner_instances raised an exception: {e}")
+
+
+def test_stop_runner_instances_failure(
+    mock_cloud_deployment_factory, mock_gh_output, mock_gh, capsys
+):
+    with patch(
+        "gha_runner.__main__.get_instance_mapping",
+        side_effect=Exception("Test"),
+    ):
+        with pytest.raises(SystemExit) as fail:
+            stop_runner_instances(
+                provider="mock_provider", cloud_params={}, gh=mock_gh
+            )
+            assert fail.type == SystemExit
+            assert fail.value.code == 1
+
+            captured = capsys.readouterr()
+            assert (
+                captured.out == "::error title=Malformed instance mapping::Test"
+            )
