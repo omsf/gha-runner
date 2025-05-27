599c090b63a3cb497403bffde849d9de5971b8cd
mihasya <m@mihasya.com>
2025-05-27 07:21:33 -0700

    paginate when fetching list of self-hosted runners

--- src/gha_runner/gh.py
+++ src/gha_runner/gh.py
@@ -206,24 +206,34 @@ class GitHubInstance:
             an error in the request or the response is not a mapping object.
         """
         runners = []
-        try:
-            res = self.get(f"repos/{self.repo}/actions/runners")
-            # This allows for arbitrary mappable objects to be used
-            if not isinstance(res, collections.abc.Mapping):
-                # This could be related to the API or the request itself.
-                # ie the response is not a JSON object
-                raise RunnerListError(f"Did not receive mapping object: {res}")
-            for runner in res["runners"]:
-                id = runner["id"]
-                name = runner["name"]
-                os = runner["os"]
-                labels = [label["name"] for label in runner["labels"]]
-                runners.append(SelfHostedRunner(id, name, os, labels))
-            return runners if len(runners) > 0 else None
-        except RuntimeError as e:
-            # This occurs when we receive a status code is > 400
-            raise RunnerListError(f"Error getting runners: {e}")
-            # Other exceptions are bubbled up to the caller
+        per_page = 30 # GH API default
+        page = 1
+        total_runners = float("inf")
+        # paginate through the pages until we have all the runners
+        while (len(runners) < total_runners):
+            try:
+                res = self.get(f"repos/{self.repo}/actions/runners?per_page={per_page}&page={page}")
+                # This allows for arbitrary mappable objects to be used
+                if not isinstance(res, collections.abc.Mapping):
+                    # This could be related to the API or the request itself.
+                    # ie the response is not a JSON object
+                    raise RunnerListError(f"Did not receive mapping object: {res}")
+                total_runners = res["total_count"]
+                page += 1
+                # protect from bug/issue where total_count is higher than actual # of runners
+                if len(res["runners"]) < 1:
+                    break
+                for runner in res["runners"]:
+                    id = runner["id"]
+                    name = runner["name"]
+                    os = runner["os"]
+                    labels = [label["name"] for label in runner["labels"]]
+                    runners.append(SelfHostedRunner(id, name, os, labels))
+            except RuntimeError as e:
+                # This occurs when we receive a status code is > 400
+                raise RunnerListError(f"Error getting runners: {e}")
+                # Other exceptions are bubbled up to the caller
+        return runners if len(runners) > 0 else None
 
     def get_runner(self, label: str) -> SelfHostedRunner:
         """Get a runner by a given label for a repository.
